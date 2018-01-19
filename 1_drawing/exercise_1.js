93b27007e391c03c9c81278fedf9371a6d6ec391
hart890 <sarahartleben@gmail.com>
2018-01-18 21:27:15 -0500

    new files

---
+++ 1_drawing/exercise_1.js
@@ -0,0 +1,16 @@
+function setup() {
+	createCanvas(200, 200);
+	background(0);
+	 
+	stroke(255);
+	strokeWeight(2);
+	 
+	fill(255, 0, 0);
+	ellipse(40, 50, 60, 60);
+	 
+	fill(0, 255, 0);
+	ellipse(80, 50, 60, 60);
+	 
+	fill(0, 0, 255);
+	ellipse(120, 50, 60, 60);
+}
---
+++ 1_drawing/stick_sketch.js
@@ -0,0 +1,6 @@
+function setup() {
+	createCanvas(200, 200);
+	background(255);
+	
+	ellipse(100, 30, 50, 50);
+}
