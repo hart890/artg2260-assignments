05ce0a90ad078f3fc94587e6f9f88f0115b666ba
hart890 <sarahartleben@gmail.com>
2018-01-24 16:06:13 -0500

    stick figure!
    booyah

--- stick_sketch.js
+++ stick_sketch.js
@@ -1,7 +1,47 @@
 function setup() {
 	createCanvas(500, 500);
 	background(0);
-	
+
+	//head
 	fill(255);
-	ellipse(250, 250, 50, 50);
+	ellipse(250, 150, 65, 65);
+
+	//body
+	fill(255);
+	rect(245, 182, 10, 150);
+
+	//arms
+	fill(255);
+	rect(255, 220, 100, 10);
+	fill(255);
+	rect(145, 220, 100, 10);
+	fill(255);
+	rect(145, 220, 10, 50);
+	fill(255);
+	rect(345, 180, 10, 50);
+
+	//hands
+	fill(255);
+	rect(345, 180, 30, 10);
+	fill(255);
+	rect(125, 265, 30, 10);
+
+
+
+	//legs
+	fill(255);
+	rect(255, 322, 100, 10);
+	fill(255);
+	rect(145, 322, 100, 10);
+	fill(255);
+	rect(345, 322, 10, 100);
+	fill(255);
+	rect(145, 322, 10, 100);
+
+	//feet
+	fill(255);
+	rect(345, 420, 30, 10);
+	fill(255);
+	rect(125, 420, 30, 10);
+
 }
