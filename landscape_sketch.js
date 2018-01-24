0f174ee96b9f0bd1c4040c5f42106e05d7b5275b
hart890 <sarahartleben@gmail.com>
2018-01-24 16:38:32 -0500

    wooooooo!

--- landscape_sketch.js
+++ landscape_sketch.js
@@ -1,7 +1,28 @@
 function setup() {
 	createCanvas(500, 500);
-	background(0);
+	background(255, 0, 102);
+	noStroke();
 	
-	fill(255);
-	ellipse(250, 250, 50, 50);
+	//sunset
+	fill(255, 128, 0);
+  	arc(250, 500, 900, 900, PI, TWO_PI);
+
+  	fill(255, 162, 0);
+  	arc(250, 500, 700, 700, PI, TWO_PI);
+
+  	//sun
+  	fill(255, 255, 0);
+  	arc(250, 500, 500, 500, PI, TWO_PI);
+
+  	//ocean
+  	fill(0, 0, 102);
+  	rect(0, 350, 500, 300);
+
+  	//ground
+  	fill(0);
+  	rect(0, 400, 500, 300);
+
+  	//mountains
+  	//triangle
+
 }
