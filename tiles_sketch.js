0fa5d2bb02cc93808fd58afd8d629ad33e06c9c6
hart890 <sarahartleben@gmail.com>
2018-02-01 17:43:47 -0500

    more tiles stuff

--- tiles_sketch.js
+++ tiles_sketch.js
@@ -22,11 +22,11 @@ var b = random(0, 255);
 
 function setup() {
 	createCanvas(800, 300);
-	background(0);
+	background(255);
 
 	for (var y = 10; y < 300; y += 10){
 		for (var x = 10; x < 800; x += 10){
-			fill(r, g, b);
+			stroke(r, g, b);
 			ellipse(x, y, tileSize, tileSize);
 		}
 	}
