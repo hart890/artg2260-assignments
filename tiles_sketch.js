bb0c30fc2ccbe56ad4fbeed20fcf06a1f0d902d0
hart890 <sarahartleben@gmail.com>
2018-02-01 17:22:22 -0500

    ah not working

--- tiles_sketch.js
+++ tiles_sketch.js
@@ -15,13 +15,18 @@
 
 
 var tileSize = 10;
+var r = random(0, 255);
+var g = random(0, 255);
+var b = random(0, 255);
+
 
 function setup() {
 	createCanvas(800, 300);
 	background(0);
-	
+
 	for (var y = 10; y < 300; y += 10){
 		for (var x = 10; x < 800; x += 10){
+			fill(r, g, b);
 			ellipse(x, y, tileSize, tileSize);
 		}
 	}
