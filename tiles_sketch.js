477c837d9db1a38dfbd686be896e375173299e76
hart890 <sarahartleben@gmail.com>
2018-02-01 17:19:25 -0500

    tiles (sort of)

--- tiles_sketch.js
+++ tiles_sketch.js
@@ -1,7 +1,28 @@
-function setup() {
-	createCanvas(500, 500);
-	background(0);
-	noStroke();
+// function setup() {
+// 	createCanvas(500, 500);
+// 	background(0);
+// 	noStroke();
 
-	//for loop
+// 	//for loop
+
+// 	//want circle
+// 	//var to change size
+// 	//something to handle clicking - change size 
+// 	//change color
+
+
+// }
+
+
+var tileSize = 10;
+
+function setup() {
+	createCanvas(800, 300);
+	background(0);
+	
+	for (var y = 10; y < 300; y += 10){
+		for (var x = 10; x < 800; x += 10){
+			ellipse(x, y, tileSize, tileSize);
+		}
+	}
 }
