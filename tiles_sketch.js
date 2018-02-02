18da850dbe8b141f521e10f942033cd54834b9d9
hart890 <sarahartleben@gmail.com>
2018-02-01 19:26:46 -0500

    tiles!

--- tiles_sketch.js
+++ tiles_sketch.js
@@ -14,20 +14,24 @@
 // }
 
 
-var tileSize = 10;
-var r = random(0, 255);
-var g = random(0, 255);
-var b = random(0, 255);
+var tileSize = 20;
+
+randomSeed(int);
 
 
 function setup() {
 	createCanvas(800, 300);
 	background(255);
 
-	for (var y = 10; y < 300; y += 10){
-		for (var x = 10; x < 800; x += 10){
-			stroke(r, g, b);
-			ellipse(x, y, tileSize, tileSize);
+	for (var y = 10; y < 300; y += 30){
+		for (var x = 10; x < 800; x += 30){
+			var r = random(1, 255);
+			var g = random(1, 255);
+			var b = random(1, 255);
+
+			fill(r, g, b);
+			noStroke();
+			rect(x, y, tileSize, tileSize);
 		}
 	}
 }
