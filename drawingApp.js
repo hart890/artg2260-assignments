2f976d0d4d015a07d987438d5be286965485068c
hart890 <sarahartleben@gmail.com>
2018-02-04 14:33:26 -0500

    Drawing App stuff

---
+++ drawingApp.js
@@ -0,0 +1,17 @@
+function setup() {
+	createCanvas(1000, 500);
+	background(0);
+}
+
+function draw() {
+
+}
+
+// function keyPressed() {
+
+// }
+
+//blue: (153, 255, 255)
+//pink: (255, 0, 127)
+//yellow: (255, 255, 0)
+//green: (153, 255, 51)
