26b5f5c8048e567f33c81fa410b7e6dcbeb15442
hart890 <sarahartleben@gmail.com>
2018-02-06 23:08:40 -0500

    changing website

---
+++ assignment_3_0/animation.js
@@ -0,0 +1,16 @@
+// Use randomness or noise
+// Use the map() function
+// Use a for loop
+// Use frameCount() to create a time-based effect
+// Use a custom function
+// Use an array
+// Use an array of objects
+
+
+function setup() {
+	createCanvas(800, 400);
+}
+
+function draw() {
+	background(0, 5, 68);
+}
