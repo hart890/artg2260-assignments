485f255a634a91c7866f9d6043e426ea83d724ca
hart890 <sarahartleben@gmail.com>
2018-03-27 08:55:10 -0400

    sound

--- assignment_9_0/sketch.js
+++ assignment_9_0/sketch.js
@@ -1,5 +1,13 @@
 var song;
 
+var navy;
+var purple;
+var cyan;
+var yellow;
+
+var c = [];
+
+
 function preload() {
 	song = loadSound("Real Love Baby.mp3");
 	//img = loadImage("misty.jpg");
@@ -21,16 +29,43 @@ function setup() {
 
 		}
 	});
+
+	x = random(0, width);
+	y = random(0, height);
+
+
 }
 
 
 function draw() {
 	background(255, 204, 255);
 
-	fill(0, 0, 204);
-	noStroke();
+	navy = color(0, 0, 204);
+	purple = color(102, 0, 204);
+	cyan = color(51, 255, 255);
+	yellow = color(255, 255, 51);
+
+	c = [navy, purple, cyan, yellow];
+
 	var level = amp.getLevel();
 	var size = map(level, 0, 1, 0, 200);
 	//image(img, size, size);
-	ellipse(width/2, height/2, size, size);
+
+	for(var i = 0; i < 100; i++) {
+
+		for(var j = 0; j < 4; j++) {
+			noStroke();
+			fill(c[j]);
+		}
+
+		//fill(c.random());
+		ellipse(x, y, size, size);
+
+		//resets
+		x = random(0, width);
+		y = random(0, height);
+
+	}
 }
+
+
